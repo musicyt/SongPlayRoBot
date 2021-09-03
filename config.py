ffee43ec82e745e863dbdf3ccb795bef6226e850
KING MUSIC <90019520+musicyt@users.noreply.github.com>
2021-09-03 10:24:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
-OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
+API_ID = int(os.getenv("7673872"))
+API_HASH = os.getenv("73c73f3a460335873090e0ab9ed691dd")
+BOT_TOKEN = os.getenv("1969809422:AAGzgX2xAg4jjWo91SJ1pqY0XA1KojnvxRc")
+DATABASE_URL = os.getenv("QBJtFGqdqhD6zGZ2DxFFlPYI5ckBL_APZQGLWP4_6puq1ACoaMsEdxMsNdK2vW4whlNq9kdiGSS1K5m1RLDeZ8JmJhekdRaFkSQTlc95Jylg_UHMmnetbbu4RZQnEv5j5UpIOk6GrBmpuoDgVsMlPIOu43R1IQpGF3wQMSg9_FFRpao9fxWksazUvtf66GskzQPBh7qx_Q83es7bvOJKrZ5nh73tTCq-qH0Mup9NJhzqYp50h2adBTt_jmmUup6pk2JwEekWC9dity4qQo24V6hJakA1I24LVTDnjLvdJ9Kqp6731l8S107ZH2d8KSbOtavzytC6lHWz_pDbaVimdfvdDlW7wA")
+OWNER_ID = list({int(x) for x in os.environ.get("1949914863", "").split()})
